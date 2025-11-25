e9d75beb45c7af35918848aeda6bb40db8146cc1
Astalakshmi Muralidharan <astalakshmimuralidharan@gmail.com>
2025-11-25 13:25:44 +0100

    Added operater

---
+++ src/main/java/org/example/operators.java
@@ -0,0 +1,16 @@
+package org.example;
+//import java.util.Scanner;
+public class operators {
+        public static void main ()
+        {
+            int a = 45;
+            int b = 11;
+            System.out.println(a + "+" + b + "=" + (a+b));
+            System.out.println(55 + "-" + 12 + " =" + (55-12));
+            System.out.println(12 + "*" + 4 + "= " + (12*4));
+            System.out.println(24 + "/" + 6 + "=" +(24/6));
+
+        }
+    }
+
+
