213d35031617a6421b6c7437820b52d0de743a04
Astalakshmi Muralidharan <astalakshmimuralidharan@gmail.com>
2025-11-25 13:44:30 +0100

    Added Avg

---
+++ src/main/java/org/example/Avg.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Avg {
+    public static void main(String[] args) {
+        int a = 23, b = 11, c = 77;
+        int average = (a + b + c) / 3;
+
+        System.out.println("(" + a + " + " + b + " + " + c + ") / 3 = " + average);
+    }
+}
--- src/main/java/org/example/Operators.java
+++ src/main/java/org/example/Operators.java
@@ -1,11 +1,10 @@
 package org.example;
 //import java.util.Scanner;
-public class operators {
+public class Operators {
         public static void main ()
         {
-            int a = 45;
-            int b = 11;
-            System.out.println(a + "+" + b + "=" + (a+b));
+
+            System.out.println(45 + "+" + 11 + "=" + (45+11));
             System.out.println(55 + "-" + 12 + " =" + (55-12));
             System.out.println(12 + "*" + 4 + "= " + (12*4));
             System.out.println(24 + "/" + 6 + "=" +(24/6));
