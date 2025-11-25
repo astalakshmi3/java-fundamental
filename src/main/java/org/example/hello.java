39e5e449c814edf27e0aace04c6de50de52b37ae
Astalakshmi Muralidharan <astalakshmimuralidharan@gmail.com>
2025-11-25 12:56:35 +0100

    Added Leap year

--- src/main/java/org/example/hello.java
+++ src/main/java/org/example/hello.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class hello {
+    public static void main(String[] args) {
+
+
+System.out.println ("Hello");
+        System.out.println("Astalakshmi");
 }
+      }
